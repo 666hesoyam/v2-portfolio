26781a5a13a00836cb7f4f6070c5fa9ef051f4fd
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2020-08-15 01:08:45 +0300

    upd portfolio and aside

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,19 +1,9 @@
-var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 1200);
-        return false;
-    });
-
-
-
 
 // Our Projects Section Slider
 $(document).ready(function(){
     $('.slider').slick({
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
         slidesToShow: 2,
         rows: 2,
         prevArrow: "<img src='img/left-arrow.svg' class='prev' alt='prev'>",
@@ -33,8 +23,8 @@ $(document).ready(function(){
 // Our Projects Section Slider
 $(document).ready(function(){
     $('.slider-two').slick({
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
         slidesToShow: 2,
         rows: 2,
         prevArrow: "<img src='img/left-arrow.svg' class='prev' alt='prev'>",
